a0759332de5f1bcdce57ba1922165910d6ae8266
Christina <belyakovachristina@rambler.ru>
2021-11-29 13:29:58 +0300

    ready for github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Figure from './components/Figure';
+import Header from './components/Header';
+import Word from './components/Word';
+import WrongLetters from './components/WrongLetters';
+import { showNotification as show } from './helpers/helpers';
+import Notification from './components/Notification';
+import Popup from './components/Popup';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      const {key, keyCode} = e;
+        if (playable && keyCode >= 65 && keyCode <= 90) {
+          const letter = key.toLowerCase();
+    
+          if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+              setCorrectLetters(correctLetters => [...correctLetters, letter]);
+            } else {
+              show(setShowNotification)
+            }
+          } else {
+            if (!wrongLetters.includes(letter)) {
+              setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+            } else {
+              show(setShowNotification)
+            }
+          }
+        }
+    }
+
+  window.addEventListener('keydown', handleKeydown);
+
+  return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    let random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
+    </>
   );
 }
 
